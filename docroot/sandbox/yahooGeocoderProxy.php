476980baf59c1cf04623a874b5239bcb2f7aaecc
Terry Smith <terry@shebiki.org>
2013-02-22 11:24:15 -0500

    Simplifies directory layout, moving /docroot/* to /*, /docroot/js/simplicity/* to /js/* and /docroot/css/simplicity/* to /css/*. Removes the bing and yahoo geocoder proxies, replacing references to them with the deployed versions on http://proxy.discoverysearchengine.com.

--- docroot/sandbox/yahooGeocoderProxy.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = 'http://where.yahooapis.com/geocode?' . $_SERVER['QUERY_STRING'];
-$c = curl_init($url);
-curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($c, CURLOPT_FAILONERROR,true);
-$response = curl_exec($c);
-echo $response;
